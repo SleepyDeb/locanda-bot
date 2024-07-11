5e7e44a190766651b86f8ea1c38c2f0bf573250b
Alessio Di Maria <alessio.dimaria@gmail.com>
2024-07-11 11:37:02 +0200

    Update index.ts

--- src/handler/commands/index.ts
+++ src/handler/commands/index.ts
@@ -1,14 +1,19 @@
 import {readdirSync} from 'fs';
 import {logger} from "../../config";
+import {Bot} from "grammy";
 
-const commands: any = {}
+export type BotHandler = (bot: Bot)=>void;
+const commands: {
+    [name: string]: BotHandler
+} = {
+}
 
 logger.info("Loading commands...")
 
 const readFile = readdirSync("./src/handler/commands")
     .filter(file => !file.endsWith("index.ts"))
 
-readFile.forEach(file => commands[file] = require(`./${file}`).default)
+readFile.forEach(file => commands[file] = require(`./${file}`).default) as BotHandler;
 
 logger.info("Commands loaded!")
 logger.debug(commands, "Commands loaded: ")
